7d52d76a65865b93f5aae6079bb6a8b15951d464
letuankhoi.dev <letuankhoi.dev@gmail.com>
2022-01-18 09:02:26 +0700

    feat: update navbar active link

--- src/components/navigation/CategoryNavigation.js
+++ src/components/navigation/CategoryNavigation.js
@@ -1,22 +1,22 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import ToggleLink from '../toggle-link/ToggleLink';
 
 export class CategoryNavigation extends Component {
   render() {
     return (
       <React.Fragment>
-        <Link className='btn btn-secondary btn-block' to={this.props.baseUrl}>
+        <ToggleLink to={this.props.baseUrl} exact={true}>
           All
-        </Link>
+        </ToggleLink>
+
         {this.props.categories &&
           this.props.categories.map((category) => (
-            <Link
-              className='btn btn-secondary btn-block'
+            <ToggleLink
               key={category}
               to={`${this.props.baseUrl}/${category.toLowerCase()}`}
             >
               {category}
-            </Link>
+            </ToggleLink>
           ))}
       </React.Fragment>
     );
